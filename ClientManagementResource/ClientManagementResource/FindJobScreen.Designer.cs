7541024fa9bca90cb7bc6379fd7e078659e0558b
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-10-03 14:17:48 -0400

    Configured job search form layout

--- ClientManagementResource/ClientManagementResource/FindJobScreen.Designer.cs
+++ ClientManagementResource/ClientManagementResource/FindJobScreen.Designer.cs
@@ -28,11 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.jobIdBox = new System.Windows.Forms.TextBox();
+            this.clientNameBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.jobGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.jobGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search By:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Job Id:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Client Name:";
+            // 
+            // jobIdBox
+            // 
+            this.jobIdBox.Location = new System.Drawing.Point(191, 10);
+            this.jobIdBox.Name = "jobIdBox";
+            this.jobIdBox.Size = new System.Drawing.Size(132, 20);
+            this.jobIdBox.TabIndex = 3;
+            // 
+            // clientNameBox
+            // 
+            this.clientNameBox.Location = new System.Drawing.Point(191, 52);
+            this.clientNameBox.Name = "clientNameBox";
+            this.clientNameBox.Size = new System.Drawing.Size(132, 20);
+            this.clientNameBox.TabIndex = 4;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(400, 50);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Search...";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // jobGridView
+            // 
+            this.jobGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.jobGridView.Location = new System.Drawing.Point(35, 95);
+            this.jobGridView.Name = "jobGridView";
+            this.jobGridView.Size = new System.Drawing.Size(464, 150);
+            this.jobGridView.TabIndex = 6;
+            // 
+            // FindJobScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 266);
+            this.Controls.Add(this.jobGridView);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.clientNameBox);
+            this.Controls.Add(this.jobIdBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FindJobScreen";
             this.Text = "FindJobScreen";
+            ((System.ComponentModel.ISupportInitialize)(this.jobGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox jobIdBox;
+        private System.Windows.Forms.TextBox clientNameBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView jobGridView;
     }
 }
